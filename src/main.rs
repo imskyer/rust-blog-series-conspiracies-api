f546ebbb06c4e08938e861f84b2b6a147cb11134
Rob Rowe <rrowe@foodlogiq.com>
2018-04-03 18:07:37 -0400

    I now have code to parse the input command line arg and the binary will display the help message if -i or --input is not provided.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate clap; // brings the clap create into scope here
+use clap::{App, Arg};     // brings the App trait from the clap create into this scope
+
 fn main() {
-    println!("Hello, world!");
+    let _matches = App::new("conspiracies-db-loader")
+       .version("0.0.1")
+       .about("Parses and stores Wikipedia conspiracy theories data")
+       .author("Rob Rowe.")
+       .arg(Arg::with_name("input")
+            .short("i")
+            .long("input")
+            .value_name("PATH")
+            .help("A path to a JSON doc ready to be parsed")
+            .takes_value(true)
+            .required(true))
+       .get_matches(); 
+
+    if let Some(path) = _matches.value_of("input") {
+        println!("A path was passed in: {} (Pretend its a path, it hasn't been tested)", path);
+    }
 }
