95a6996526c68d3c7f39a83126b436fc8136e7c6
Rob Rowe <rrowe@foodlogiq.com>
2018-04-04 07:34:26 -0400

    cmd line is parsed and value of --input is now being validated to ensure it is a real path

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate clap; // brings the clap create into scope here
 use clap::{App, Arg};     // brings the App trait from the clap create into this scope
+use std::path::Path;  // Path is part of the standard lib so we only need the `use` statement to bring it into scope
 
 fn main() {
     let _matches = App::new("conspiracies-db-loader")
@@ -15,7 +16,16 @@ fn main() {
             .required(true))
        .get_matches(); 
 
-    if let Some(path) = _matches.value_of("input") {
+    if let Some(path) = _matches.value_of("input")  {
         println!("A path was passed in: {} (Pretend its a path, it hasn't been tested)", path);
-    }
+        if !Path::new(path).exists() {
+            println!("Path does not exst")
+        }
+
+        parse_json_file();
+    } 
+}
+
+fn parse_json_file() {
+    println!("This is a placeholder in the parse_json_file");
 }
