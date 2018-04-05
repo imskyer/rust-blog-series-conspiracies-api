589d9f8358361f0fae33ffc598d5048b8c1f7e3c
Rob Rowe <rrowe@foodlogiq.com>
2018-04-05 11:15:34 -0400

    initial functioning code that reads the file's conetnts in

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-extern crate clap; // brings the clap create into scope here
-use clap::{App, Arg};     // brings the App trait from the clap create into this scope
-use std::path::Path;  // Path is part of the standard lib so we only need the `use` statement to bring it into scope
+// brings the clap create into scope here
+extern crate clap; 
+// brings the App trait from the clap create into this scope
+use clap::{App, Arg};
+// Path is part of the standard lib so we only need the 
+// `use` statement to bring it into scope 
+use std::path::Path;
+// for the process::exit call in the main fn
+use std::process;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     let _matches = App::new("conspiracies-db-loader")
        .version("0.0.1")
        .about("Parses and stores Wikipedia conspiracy theories data")
-       .author("Rob Rowe.")
+       .author("Rob Rowe")
        .arg(Arg::with_name("input")
             .short("i")
             .long("input")
@@ -16,16 +24,30 @@ fn main() {
             .required(true))
        .get_matches(); 
 
-    if let Some(path) = _matches.value_of("input")  {
-        println!("A path was passed in: {} (Pretend its a path, it hasn't been tested)", path);
-        if !Path::new(path).exists() {
-            println!("Path does not exst")
+    if let Some(input_value) = _matches.value_of("input")  {
+        
+        let path = Path::new(input_value);
+        if !path.exists() {
+            println!("--input value is not a valid path");
+            process::exit(1);
         }
 
-        parse_json_file();
-    } 
-}
+        if path.is_dir() {
+            println!("--input value is a path to a directory, not a file.");
+            process::exit(1);
+        }
 
-fn parse_json_file() {
-    println!("This is a placeholder in the parse_json_file");
+        let contents = read_file(input_value);
+        println!("With text:\n{}", contents);
+    } 
+} 
+
+fn read_file(filepath: &str) -> String {
+    //Here's an example of reading a file
+    let mut f = File::open(filepath).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    return contents;
 }
